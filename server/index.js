6a798a957de425d4e3b2d84a36b0d8bab3fd4c51
Swapan Jain <96644660+swapthe192002@users.noreply.github.com>
2024-02-01 11:52:37 +0530

    Update index.js
    updated cors origin policy

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const { Server } = require("socket.io");
 
 const io = new Server(8000, {
-  cors: true,
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"]
+  }
 });
 
 const emailToSocketIdMap = new Map();
